95e9ccbbf755c827438228ec498c9c602c129357
NitinAkode-1 <nitinakode@gmail.com>
2024-10-28 01:01:27 +0530

    yes

--- src/Complete.tsx
+++ src/Complete.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { Container, Box } from '@mui/material';
+import {  Box } from '@mui/material';
 import About from './About';
 import Experience from './Experince';
 import Footer from './Footer';
-import Header from './Header';
+
 import Home from './Home';
 import Projects from './Projects';
 import Skills from './skills';
